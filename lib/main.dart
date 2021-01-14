72713e6cab6c85cbe3be5517774f61d8422040b2
syazwanirahimin <196359@student.upm.edu.my>
2021-01-14 15:56:13 +0800

    add firebase authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:menuyo/providers/Cart.dart';
-import 'package:menuyo/screens/cart_screen.dart';
-import 'package:menuyo/screens/review_screen.dart';
-import 'package:provider/provider.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-import 'screens/categories_screen.dart';
-import 'screens/category_meals_screen.dart';
-import 'screens/meal_detail_screen.dart';
-import 'widgets/main_drawer.dart';
-import 'screens/scan_screen.dart';
-import 'models/meal.dart';
-import 'models/category.dart';
+import 'home.dart';
+import 'auth/signup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<Meal> meals = new List<Meal>();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => Cart(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            primarySwatch: Colors.pink,
-            accentColor: Colors.amber,
-            canvasColor: Color.fromRGBO(255, 254, 229, 1),
-            fontFamily: 'Raleway',
-            textTheme: ThemeData.light().textTheme.copyWith(
-                bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                headline5: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'RobotoCondensed',
-                    fontWeight: FontWeight.bold))),
-        routes: {
-          '/': (ctx) => this.meals.isEmpty
-              ? ScanScreen(this.meals)
-              : CategoriesScreen(this.meals[0].categories as List<Category>),
-          CategoryMealsScreen.routName: (ctx) => CategoryMealsScreen(),
-          CartScreen.routeName: (ctx) => CartScreen(),
-          MealDetailScreen.routName: (ctx) => MealDetailScreen(),
-          MainDrawer.routName: (ctx) => MainDrawer(),
-          ScanScreen.routName: (ctx) => ScanScreen(this.meals),
-          ReviewScreen.routeName: (ctx) => ReviewScreen(),
-          CategoriesScreen.routName: (ctx) => CategoriesScreen(
-              this.meals.isNotEmpty
-                  ? this.meals[0].categories as List<Category>
-                  : null),
-        },
-        onUnknownRoute: (settings) {
-          return MaterialPageRoute(
-              builder: (ctx) => CategoriesScreen(this.meals.isNotEmpty
-                  ? this.meals[0].categories as List<Category>
-                  : null));
-        },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Menuyo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: IntroScreen(),
     );
+  }
+}
+
+class IntroScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    User result = FirebaseAuth.instance.currentUser;
+    return new SplashScreen(
+        navigateAfterSeconds: result != null ? Home(uid: result.uid) : SignUp(),
+        seconds: 5,
+        title: new Text(
+          'Welcome To Menuyo!',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        image: Image.asset('assets/images/dart.png', fit: BoxFit.scaleDown),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: () => print("flutter"),
+        loaderColor: Colors.red);
   }
 }
